b66c3d32addd98312343a205d06e46837a0ead88
NamNguyen2710 <nam271000@gmail.com>
2023-05-20 11:18:14 +0700

    add routing

---
+++ src/controller/api/product/product.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { protectHttp } from '../auth/auth.controller.js';
+import { httpCreateProduct, httpReadProducts } from './product.controller.js';
+
+const router = express.Router();
+router
+  .get(protectHttp(['vendor', 'customer']), httpReadProducts)
+  .post(protectHttp(['vendor']), httpCreateProduct);
+
+export default router;
