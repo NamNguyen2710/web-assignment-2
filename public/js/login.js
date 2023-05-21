9e9df870ce7aa13ab12d409aaa28ce2891cfc090
NamNguyen2710 <nam271000@gmail.com>
2023-05-21 21:40:13 +0700

    fix login logic

--- public/js/login.js
+++ public/js/login.js
@@ -4,11 +4,13 @@ const errorText = document.querySelector('p#error-text');
 form.addEventListener('submit', e => {
   e.preventDefault();
   const formData = new FormData(form);
+  const data = {};
+  formData.forEach((value, key) => (data[key] = value));
 
   fetch('/api/users/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(formData)
+    body: JSON.stringify(data)
   }).then(async res => {
     if (res.status === 200) return window.location.replace('/my-account');
 
