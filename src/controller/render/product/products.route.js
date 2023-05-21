f49b2a9d1fd23bef5fc0a15fa3464322b05e0e85
NamNguyen2710 <nam271000@gmail.com>
2023-05-21 12:12:55 +0700

    update file structure

---
+++ src/controller/render/product/products.route.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import authController from '../user/auth.controller.js';
+import {
+  productListController,
+  productDetailController,
+  createProductController
+} from './render/products/products.controller.js';
+
+const router = express.Router();
+
+router.get('/new', authController(['vendor']), createProductController);
+router.get(
+  '/:id',
+  authController(['vendor', 'customer']),
+  productDetailController
+);
+router.get('/', authController(['vendor']), productListController);
+
+export default router;
