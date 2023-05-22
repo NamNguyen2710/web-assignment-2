254370b84353d26afc72d66491d1d6d2e14758a8
NamNguyen2710 <nam271000@gmail.com>
2023-05-22 13:52:28 +0700

    update template

---
+++ public/js/myAccount.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form');
+const avatar = document.querySelector('img#avatar');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  fetch('/api/users/account-info', {
+    method: 'PUT',
+    body: formData
+  }).then(async res => {
+    if (res.status === 200) {
+      const data = await res.json();
+      avatar.src = `/public/image/${data.avatar}`;
+    }
+  });
+});
