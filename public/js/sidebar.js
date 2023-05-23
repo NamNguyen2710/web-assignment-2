9f68fab993082c8b1b2d4abcca6e394a22faa6be
NamNguyen2710 <nam271000@gmail.com>
2023-05-23 17:04:52 +0700

    add comment

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,3 +1,10 @@
+// RMIT University Vietnam
+// Course: COSC2430 Web Programming
+// Semester: 2023A
+// Assessment: Assignment 2
+// Authors: Nguyen Ngo Hoang Nam, Tran Tuan Bao, Nguyen Huy Anh, Duong Quang Nhat Bao
+// IDs: s3980297, s3970254, s3956092s, s3987239
+// Acknowledgement: Acknowledge the resources that you use here.
 /* Set the width of the side navigation to 250px */
 const toggleBtn = document.querySelector('button.sidebar-toggle');
 toggleBtn.addEventListener('click', function () {
